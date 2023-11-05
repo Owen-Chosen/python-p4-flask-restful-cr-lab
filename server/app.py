829e06afe55baa82ecf0fa0789ea1b0057e632fd
owen <owen.chosen@gmail.com>
2023-11-05 17:36:37 +0100

    Done

--- server/app.py
+++ server/app.py
@@ -9,7 +9,7 @@ from models import db, Plant
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = True
+app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
@@ -17,10 +17,29 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    
+    def get(self):
+        plant_dict = [plant.to_dict() for plant in Plant.query.all()]
+        return make_response(jsonify(plant_dict), 200)
+    
+    def post(self):
+        new_plant = Plant(name=request.form.get('name'),
+                          image=request.form.get('image'),
+                          price=request.form.get('price'))
+        db.session.add(new_plant)
+        db.session.commit()
+        return make_response(jsonify(new_plant.to_dict()), 201)
+    
+api.add_resource(Plants, ('/plants'))
+
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+        plant_by_id = Plant.query.filter_by(id==id).first()
+        plant_by_id_dict = plant_by_id.to_dict()
+        return make_response(jsonify(plant_by_id_dict), 200)
+    
+api.add_resource(PlantByID, ('/plants/<int:id>'))
         
 
 if __name__ == '__main__':
